37ff0e4473e8a997fa8eb1be244ff8fd12772a36
Timur Karimov <timur.karimow.95@gmail.com>
2018-10-05 16:07:04 +0200

    Revert "Add heroesService and messageService"

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HeroService} from '../hero.service';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +8,13 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
+  heroes = HEROES;
   selectedHero: Hero;
-  constructor(private heroService: HeroService) { }
-  ngOnInit() {
-    this.getHeroes();
-  }
+  constructor() { }
+  ngOnInit() {}
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
+
+
 }
